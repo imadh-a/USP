56b8a008bd78f0a34b81d85f4294ac9c40bf8401
imadh-a <93546632+imadh-a@users.noreply.github.com>
2022-12-13 14:00:42 +0530

    Add files via upload

---
+++ Week-2/area.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Enter radius of circle"
+read r
+echo "Area is equal to:"
+echo "3.14 * $r *$r"|bc
+
---
+++ Week-2/dfiles.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Enter name of directory "
+read dir
+
+echo "display files in $dir: `ls /home/bmsce/$dir`"
