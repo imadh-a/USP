518b5c45dc10873cf6c4935cb7e5f1d5bbcfb693
imadh-a <93546632+imadh-a@users.noreply.github.com>
2023-01-29 23:40:47 +0530

    Update leap.sh

--- Week-3/leap.sh
+++ Week-3/leap.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-echo "Enter a Year: "
+echo "Enter year"
 read year
-
-if [ $((year % 4)) -eq 0 ]
+if [  $((year % 4)) -eq 0 ] && [ $((year%100)) -ne 0 ] || [ $((year%400)) -eq 0 ]
 then
-  if [ $((year % 100)) -eq 0 ]
-    then
-    if [ $((year % 400)) -eq 0 ]
-          then
-        echo "$year is a leap year"
-    else
-           echo "$year is not a leap year"
-    fi
-  else
-  echo "$year is a leap year"
-  fi
+echo "$year is a leap year"
 else
- echo "$year is not a leap year"
-
+echo "$year is not a leap year"
 fi
